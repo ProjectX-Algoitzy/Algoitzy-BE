52a4317f0583d712df0b00020a92dfbfa5ec54eb
Kim Doo Hyeon <engus525@naver.com>
2024-04-07 02:10:45 +0900

    [REFACTOR] 관리자, 스터디원 로그인 API 모듈 구분 #4

---
+++ module-core/src/main/java/org/example/domain/member/service/LoginService.java
@@ -0,0 +1,37 @@
+package org.example.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.member.controller.request.LoginRequest;
+import org.example.domain.member.controller.response.LoginResponse;
+import org.example.security.jwt.JwtToken;
+import org.example.security.jwt.JwtTokenProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LoginService {
+
+  private final AuthenticationManagerBuilder authenticationManagerBuilder;
+  private final JwtTokenProvider jwtTokenProvider;
+
+  /**
+   * 로그인
+   */
+  public LoginResponse login(LoginRequest request) {
+    UsernamePasswordAuthenticationToken authenticationToken =
+      new UsernamePasswordAuthenticationToken(request.email(), request.password());
+    Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+    JwtToken jwtToken = jwtTokenProvider.generateToken(authentication, request.email());
+
+    return LoginResponse
+      .builder()
+      .accessToken(jwtToken.accessToken())
+      .build();
+  }
+
+}
--- module-user/src/main/java/org/example/domain/member/service/CreateMemberService.java
+++ module-user/src/main/java/org/example/domain/member/service/CreateMemberService.java
@@ -3,22 +3,15 @@ package org.example.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import org.example.api_response.exception.GeneralException;
 import org.example.api_response.status.ErrorStatus;
-import org.example.domain.member.Member;
 import org.example.domain.member.controller.request.CreateMemberRequest;
-import org.example.domain.member.controller.request.LoginRequest;
 import org.example.domain.member.controller.request.ValidateEmailRequest;
 import org.example.domain.member.controller.request.ValidateHandleRequest;
-import org.example.domain.member.controller.response.LoginResponse;
+import org.example.domain.member.Member;
 import org.example.domain.member.enums.Role;
 import org.example.domain.member.repository.MemberRepository;
-import org.example.security.jwt.JwtToken;
-import org.example.security.jwt.JwtTokenProvider;
 import org.example.util.http_request.HttpRequest;
 import org.example.util.http_request.Url;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,9 +23,6 @@ public class CreateMemberService {
 
   private final MemberRepository memberRepository;
   private final PasswordEncoder encoder;
-  private final AuthenticationManagerBuilder authenticationManagerBuilder;
-  private final JwtTokenProvider jwtTokenProvider;
-
   /**
    * 회원 가입
    */
@@ -64,20 +54,4 @@ public class CreateMemberService {
       throw new GeneralException(ErrorStatus.BAD_REQUEST, "백준 핸들이 유효하지 않습니다.");
     }
   }
-
-  /**
-   * 로그인
-   */
-  public LoginResponse login(LoginRequest request) {
-    UsernamePasswordAuthenticationToken authenticationToken =
-      new UsernamePasswordAuthenticationToken(request.email(), request.password());
-    Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-    JwtToken jwtToken = jwtTokenProvider.generateToken(authentication, request.email());
-
-    return LoginResponse
-      .builder()
-      .accessToken(jwtToken.accessToken())
-      .build();
-  }
-
 }
