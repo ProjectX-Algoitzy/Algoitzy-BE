5af02ae6b312407579a531973670168c3f3eba64
Kim Doo Hyeon <engus525@naver.com>
2024-04-22 00:32:16 +0900

    [FEATURE] (Common) 지원서 목록 열람 #15 - 충돌 해결

--- module-core/src/main/java/org/example/domain/answer/Answer.java
+++ module-core/src/main/java/org/example/domain/answer/Answer.java
@@ -48,9 +48,6 @@ public class Answer {
   @OneToMany(mappedBy = "answer", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<SelectAnswer> selectAnswerList = new ArrayList<>();
 
-  @Setter
-  @OneToMany(mappedBy = "answer", cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<SelectAnswer> selectAnswerList = new ArrayList<>();
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "application_id")
   private Application application;
