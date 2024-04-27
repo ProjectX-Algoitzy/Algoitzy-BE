30c5f1e1147f01cc975dc9d6372ebf721a8ccc91
Kim Doo Hyeon <engus525@naver.com>
2024-04-27 16:01:55 +0900

    [FEATURE] (Common) 지원서 상세 열람 #51
    - 시간 array 형태로 변환 차단

--- module-user/src/main/java/org/example/config/SwaggerConfig.java
+++ module-user/src/main/java/org/example/config/SwaggerConfig.java
@@ -1,16 +1,32 @@
 package org.example.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+  @Override
+  protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+      .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+      .build();
+    converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+    super.configureMessageConverters(converters);
+  }
 
   @Bean
   public OpenAPI KoalaServerAPI() {
