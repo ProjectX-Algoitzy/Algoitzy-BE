bd487f9bde31c3eaadbe7516b497ed08badc911d
Kim Doo Hyeon <engus525@naver.com>
2024-05-09 22:46:18 +0900

    [FIX] 지원서 생성 시 문항 없으면 empty list

--- module-admin/src/main/java/org/example/domain/application/controller/request/CreateApplicationRequest.java
+++ module-admin/src/main/java/org/example/domain/application/controller/request/CreateApplicationRequest.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
+import lombok.Builder;
 import org.example.domain.select_question.controller.request.CreateSelectQuestionRequest;
 import org.example.domain.text_question.controller.request.CreateTextQuestionRequest;
 
@@ -26,4 +28,13 @@ public record CreateApplicationRequest(
   List<@Valid CreateSelectQuestionRequest> createSelectQuestionRequestList
 ) {
 
+  @Builder
+  public CreateApplicationRequest {
+    if (createTextQuestionRequestList == null) {
+      createTextQuestionRequestList = new ArrayList<>();
+    }
+    if (createSelectQuestionRequestList == null) {
+      createSelectQuestionRequestList = new ArrayList<>();
+    }
+  }
 }
