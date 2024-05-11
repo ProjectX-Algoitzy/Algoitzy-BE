fba065d9eb0223cc650bc7a0d3b8af9aa651d0d6
Kim Doo Hyeon <engus525@naver.com>
2024-05-11 18:33:26 +0900

    [REFACTOR] 작성한 지원서 목록 학년, 학과, 전형 단계, 면접 일정 추가 #69
    - HealthCheckController hidden true

--- module-user/src/main/java/org/example/health/HealthCheckController.java
+++ module-user/src/main/java/org/example/health/HealthCheckController.java
@@ -1,5 +1,6 @@
 package org.example.health;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     @GetMapping("/health")
+    @Operation(hidden = true)
     public String healthCheck() {
         return "I'm healthy";
     }
