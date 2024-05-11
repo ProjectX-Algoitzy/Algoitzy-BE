5b822f083306c7f32b28644a74da36cc287b732a
Kim Doo Hyeon <engus525@naver.com>
2024-05-12 04:43:26 +0900

    [FEATURE] 이메일 전송 #72
    - 전형 단계별 이메일 전송

--- module-core/src/main/java/org/example/email/enums/EmailType.java
+++ module-core/src/main/java/org/example/email/enums/EmailType.java
@@ -11,19 +11,32 @@ import org.example.api_response.status.ErrorStatus;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum EmailType {
+
+  // 회원가입 인증
   CERTIFICATION("KOALA 이메일 인증 코드", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/certification.html"),
-  DOCUMENT_RESULT("KOALA 서류 전형 안내", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/certification.html"),
-  INTERVIEW_DATE("KOALA 면접 일정 안내", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/certification.html"),
-  INTERVIEW_RESULT("KOALA 면접 결과 발표", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/certification.html");
+
+  // 지원 전형 안내
+  DOCUMENT_PASS("KOALA 서류 전형 안내", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/document-pass.html"),
+  DOCUMENT_FAIL("KOALA 서류 전형 안내", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/document-fail.html"),
+  INTERVIEW("KOALA 면접 일정 안내", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/interview.html"),
+  FAIL("KOALA 면접 결과 발표", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/fail.html"),
+  PASS("KOALA 면접 결과 발표", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/pass.html"),
+
+  // 비밀번호 찾기
+  FIND_PASSWORD("KOALA 비밀번호 안내", "https://kau-koala.s3.ap-northeast-2.amazonaws.com/certification.html");
 
   private String subject;
   private String path;
 
+  public static boolean exist(String emailType) {
+    return Arrays.stream(values()).anyMatch(type -> type.toString().equals(emailType));
+  }
+
   public static String getSubject(String emailType) {
     return Arrays.stream(EmailType.values())
-      .filter(type -> type.subject.equals(emailType))
-      .findFirst().orElseThrow(() ->
-        new GeneralException(ErrorStatus.BAD_REQUEST, "Email Type이 존재하지 않습니다."))
+      .filter(type -> type.toString().equals(emailType))
+      .findFirst()
+      .orElseThrow(() -> new GeneralException(ErrorStatus.BAD_REQUEST, "Not Found Enum Value : " + emailType))
       .subject;
   }
 }
