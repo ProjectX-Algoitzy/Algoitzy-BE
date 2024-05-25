45a7b3056fae16db5ec5acea507b8ad87d94224a
Kim Doo Hyeon <engus525@naver.com>
2024-05-26 02:01:46 +0900

    [REFACTOR] 로그인 API 분리 #93
    - 지원서 양식 목록 interviewId 추가

--- module-core/src/main/java/org/example/domain/answer/controller/response/ListAnswerDto.java
+++ module-core/src/main/java/org/example/domain/answer/controller/response/ListAnswerDto.java
@@ -32,6 +32,9 @@ public class ListAnswerDto {
   @Schema(description = "전형 단계")
   private String status;
 
+  @Schema(description = "면접 ID")
+  private long interviewId;
+
   @Schema(description = "면접 일자")
   private String interviewTime;
 
