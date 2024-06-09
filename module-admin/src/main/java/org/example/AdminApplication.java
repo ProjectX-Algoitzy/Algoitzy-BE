40f1955f3af56a8c04bdcbb483cf783e02cbf635
sebin chu <cntpqls0218@gmail.com>
2024-06-09 16:33:49 +0900

    [FEATURE] solved.ac api 요청을 위한 FeignClient 정의 #99
    - https://solved.ac/api/v3/search/problem으로 요청
    - 문제 검색 api를 통한 문제 리스트 받기

--- module-admin/src/main/java/org/example/AdminApplication.java
+++ module-admin/src/main/java/org/example/AdminApplication.java
@@ -2,12 +2,14 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableFeignClients
+@SpringBootApplication
 public class AdminApplication {
 
   public static void main(String[] args) {
