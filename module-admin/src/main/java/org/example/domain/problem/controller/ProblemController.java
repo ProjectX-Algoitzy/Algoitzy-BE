40d7db080a399e90706a2f36db25b56b61eeab7f
sebin chu <cntpqls0218@gmail.com>
2024-06-11 23:58:48 +0900

    [FEATURE] solved.ac api 요청을 위한 FeignClient 정의 #99
    - gzip 오류 코드 unversioned

---
+++ module-admin/src/main/java/org/example/domain/problem/controller/ProblemController.java
@@ -0,0 +1,28 @@
+package org.example.domain.problem.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.domain.problem.response.ProblemResponse;
+import org.example.domain.problem.service.CreateProblemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/crawling")
+@Tag(name = "ProblemController", description = "[ADMIN] 백준 문제 크롤링 관련 API")
+public class ProblemController {
+
+    private final CreateProblemService createProblemService;
+
+    @GetMapping("/problems")
+    @Operation(summary = "백준 문제 크롤링")
+    public void getProblemList() {
+        createProblemService.searchProblems();
+    }
+
+}
