33990d0d9c8b3d538a47de708de3da1f4e72f941
sebin chu <cntpqls0218@gmail.com>
2024-06-13 22:45:14 +0900

    [FEATURE] solved.ac api 요청 및 DB 저장을 위한 Problem 로직 구현 #99
    - page 업데이트 구현

--- module-admin/src/main/java/org/example/domain/problem/service/CrawlingProblemService.java
+++ module-admin/src/main/java/org/example/domain/problem/service/CrawlingProblemService.java
@@ -2,6 +2,8 @@ package org.example.domain.problem.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.domain.problem.Problem;
+import org.example.domain.problem.repository.ProblemRepository;
 import org.example.schedule.SolvedAcProblemSearchClient;
 import org.example.schedule.solved_ac_response.ProblemDto;
 import org.example.schedule.solved_ac_response.ProblemResponse;
@@ -14,15 +16,28 @@ import org.springframework.stereotype.Service;
 public class CrawlingProblemService {
 
     private final SolvedAcProblemSearchClient solvedAcProblemSearchClient;
+    private final ProblemRepository problemRepository;
 
     public List<ProblemDto> searchProblems() {
 
-        int page = 1;
+        int page = getTodoPageNumber();
         String query = "*b..*d";
         String sort = "id";
         String direction = "asc";
-
         ProblemResponse problemResponse = solvedAcProblemSearchClient.searchProblems(page, query, sort, direction);
+
         return problemResponse.getProblemList();
+    }
+
+    private int getTodoPageNumber() {
+        Long latestProblemId =  problemRepository.findTopByOrderByIdDesc()
+            .map(Problem::getId)
+            .orElse(0L);
+
+        if (latestProblemId == 0) {
+            return 1;
+        } else {
+            return  (int) (latestProblemId/50);
+        }
     }
 }
