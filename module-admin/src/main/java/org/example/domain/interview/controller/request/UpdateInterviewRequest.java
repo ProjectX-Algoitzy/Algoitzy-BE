371893f58ba5e0299adcf652f89a9a7274c14684
Kim Doo Hyeon <engus525@naver.com>
2024-06-21 00:45:06 +0900

    [FEATURE] 정규 스터디 상세 조회 #65
    - 면접 일정 수정 요청 형식 대응

--- module-admin/src/main/java/org/example/domain/interview/controller/request/UpdateInterviewRequest.java
+++ module-admin/src/main/java/org/example/domain/interview/controller/request/UpdateInterviewRequest.java
@@ -1,9 +1,9 @@
 package org.example.domain.interview.controller.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Schema(description = "면접 일정 수정 요청 객체")
 public record UpdateInterviewRequest(
@@ -13,17 +13,10 @@ public record UpdateInterviewRequest(
   Long interviewId,
 
   @NotBlank
-  @Schema(description = "면접 시간")
+  @Length(min = 8, max = 8)
+  @Schema(description = "면접 시간", example = "05251200")
   String time
 
 ) {
-
-  @AssertTrue(message = "MM월 DD일 HH:MM 형식이어야 합니다.")
-  public boolean getTimeValidate() {
-    return time.length() >= 10
-      && time.contains("월")
-      && time.contains("일")
-      && time.contains(":");
-  }
 
 }
--- module-admin/src/main/java/org/example/domain/interview/service/CreateInterviewService.java
+++ module-admin/src/main/java/org/example/domain/interview/service/CreateInterviewService.java
@@ -1,6 +1,11 @@
 package org.example.domain.interview.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
+import org.example.api_response.exception.GeneralException;
+import org.example.api_response.status.ErrorStatus;
 import org.example.domain.interview.Interview;
 import org.example.domain.interview.controller.request.UpdateInterviewRequest;
 import org.springframework.stereotype.Service;
@@ -18,6 +23,14 @@ public class CreateInterviewService {
    */
   public void updateInterview(UpdateInterviewRequest request) {
     Interview interview = coreInterviewService.findById(request.interviewId());
-    interview.updateTime(request.time());
+
+    try {
+      SimpleDateFormat inputFormat = new SimpleDateFormat("MMddHHmm");
+      Date date = inputFormat.parse(request.time());
+      SimpleDateFormat outputFormat = new SimpleDateFormat("M월 d일 HH:mm");
+      interview.updateTime(outputFormat.format(date));
+    } catch (ParseException e) {
+      throw new GeneralException(ErrorStatus.INTERNAL_ERROR, "면접 일정 변환 중 오류가 발생했습니다.");
+    }
   }
 }
