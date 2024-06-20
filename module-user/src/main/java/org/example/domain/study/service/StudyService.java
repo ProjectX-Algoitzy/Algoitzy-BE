b3ae64fbf6de49a014e1cca785a3ec01151d22b0
Kim Doo Hyeon <engus525@naver.com>
2024-06-20 23:23:30 +0900

    [FEATURE] 정규 스터디 상세 조회 #65
    - controller frame

--- module-user/src/main/java/org/example/domain/study/service/StudyService.java
+++ module-user/src/main/java/org/example/domain/study/service/StudyService.java
@@ -1,9 +1,13 @@
 package org.example.domain.study.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.domain.attendance.controller.response.ListAttendanceResponse;
+import org.example.domain.curriculum.controller.response.ListCurriculumResponse;
 import org.example.domain.study.controller.request.CreateTempStudyRequest;
 import org.example.domain.study.controller.response.DetailTempStudyResponse;
+import org.example.domain.study.controller.response.ListRegularStudyResponse;
 import org.example.domain.study.controller.response.ListTempStudyResponse;
+import org.example.domain.study.controller.response.RegularStudyInfoResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -47,5 +51,40 @@ public class StudyService {
    */
   public DetailTempStudyResponse getTempStudy(Long studyId) {
     return detailStudyService.getTempStudy(studyId);
+  }
+
+  /**
+   * 정규 스터디 목록 조회
+   */
+  public ListRegularStudyResponse getRegularStudyList() {
+    return null;
+  }
+
+  /**
+   * 정규 스터디 정보 조회
+   */
+  public RegularStudyInfoResponse getRegularStudyInfo() {
+    return null;
+  }
+
+  /**
+   * 정규 스터디 홈 조회
+   */
+  public String getContent(Long studyId) {
+    return null;
+  }
+
+  /**
+   * 정규 스터디 커리큘럼 조회
+   */
+  public ListCurriculumResponse getCurriculumList(Long studyId) {
+    return null;
+  }
+
+  /**
+   * 정규 스터디 출석부 조회
+   */
+  public ListAttendanceResponse getAttendanceList(Long studyId) {
+    return null;
   }
 }
