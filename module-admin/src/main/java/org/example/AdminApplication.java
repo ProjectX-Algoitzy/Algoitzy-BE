f4a4035144a0b470e436e9bce1cc7aa43f737101
Kim Doo Hyeon <engus525@naver.com>
2024-06-22 18:24:58 +0900

    [FEATURE] 정규 스터디 상세 조회 #65
    - TimeZone 설정
    - Log 출력 시간 설정

--- module-admin/src/main/java/org/example/AdminApplication.java
+++ module-admin/src/main/java/org/example/AdminApplication.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 public class AdminApplication {
+
+  @PostConstruct
+  public void setTimeZone() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(AdminApplication.class, args);
