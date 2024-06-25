82b78be38e560d75c346ce1bd2932914c38eafc2
Kim Doo Hyeon <engus525@naver.com>
2024-06-26 02:15:25 +0900

    [REFACTOR] 프론트엔드 에디터 대응 #102
    - 스터디 프로필 추가
    - 토큰 유효시간 int 초과 오류 수정

--- module-admin/src/main/java/org/example/domain/application/controller/response/ListApplicationByGenerationDto.java
+++ module-admin/src/main/java/org/example/domain/application/controller/response/ListApplicationByGenerationDto.java
@@ -24,6 +24,9 @@ public class ListApplicationByGenerationDto {
   @Schema(description = "대상 스터디 이름")
   private String studyName;
 
+  @Schema(description = "대상 스터디 이미지")
+  private String studyProfileUrl;
+
   @Schema(description = "확정 여부")
   private boolean confirmYN;
 
