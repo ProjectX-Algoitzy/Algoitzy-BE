f497f29c6ea3e66b1aa5b53d1a6bc1bc4953e2aa
Kim Doo Hyeon <engus525@naver.com>
2024-07-09 19:21:55 +0900

    [FEATURE] user 출석부 등록 #139
    - 번호 유효성 검사 수정

--- module-core/src/main/java/org/example/validator/PhoneNumberValidator.java
+++ module-core/src/main/java/org/example/validator/PhoneNumberValidator.java
@@ -10,9 +10,7 @@ public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber, St
   public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
     if (!StringUtils.hasText(phoneNumber)) return false;
 
-    // 010-1234-1234 형식만 허용
-    if (phoneNumber.charAt(3) != '-' || phoneNumber.charAt(8) != '-') return false;
-    phoneNumber = phoneNumber.replaceAll("-", "");
+    // 01012341234 형식만 허용
     if (phoneNumber.length() != 11) return false;
 
     return phoneNumber.matches("[0-9]+");
