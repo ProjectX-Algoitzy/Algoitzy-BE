0a98bb52941c7c722243d5a99723bcf320e145d9
Kim Doo Hyeon <engus525@naver.com>
2024-07-16 23:44:17 +0900

    [FEATURE] 정규 스터디 상세 조회 #65
    - @Default

--- module-user/src/main/java/org/example/domain/attendance/controller/response/ListAttendanceResponse.java
+++ module-user/src/main/java/org/example/domain/attendance/controller/response/ListAttendanceResponse.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,5 +16,6 @@ import lombok.NoArgsConstructor;
 @Schema(description = "출석부 목록 응답 객체")
 public class ListAttendanceResponse {
 
+  @Default
   private List<ListAttendanceDto> attendanceList = new ArrayList<>();
 }
