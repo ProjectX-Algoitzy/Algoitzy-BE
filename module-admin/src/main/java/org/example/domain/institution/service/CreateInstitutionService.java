6d7469eaa781f96eaa26c91df36b11750e7f26b6
Kim Doo Hyeon <engus525@naver.com>
2024-07-27 20:20:31 +0900

    [FEATURE] 기업/부트캠프 문제집 CRUD #181
    - UpdateInstitutionRequest enum 값으로 받도록 수정

--- module-admin/src/main/java/org/example/domain/institution/service/CreateInstitutionService.java
+++ module-admin/src/main/java/org/example/domain/institution/service/CreateInstitutionService.java
@@ -6,7 +6,6 @@ import org.example.api_response.status.ErrorStatus;
 import org.example.domain.institution.Institution;
 import org.example.domain.institution.controller.request.CreateInstitutionRequest;
 import org.example.domain.institution.controller.request.UpdateInstitutionRequest;
-import org.example.domain.institution.enums.InstitutionType;
 import org.example.domain.institution.repository.InstitutionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,7 +43,7 @@ public class CreateInstitutionService {
     institution.update(
       request.name(),
       request.content(),
-      InstitutionType.valueOf(request.type())
+      request.type()
     );
   }
 
--- module-core/src/main/java/org/example/domain/institution/enums/InstitutionType.java
+++ module-core/src/main/java/org/example/domain/institution/enums/InstitutionType.java
@@ -1,12 +1,7 @@
 package org.example.domain.institution.enums;
 
-import java.util.Arrays;
-
 public enum InstitutionType {
   COMPANY,
-  CAMP;
+  CAMP
 
-  public static boolean exist(String institutionType) {
-    return Arrays.stream(values()).anyMatch(type -> type.toString().equals(institutionType));
-  }
 }
