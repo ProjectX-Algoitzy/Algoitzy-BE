b16d224191b44970f315f673477e9201532caeef
Kim Doo Hyeon <engus525@naver.com>
2024-07-29 21:44:24 +0900

    [FIX] USER 스터디 상세 스터디원만 열람 가능하도록 #192
    - 모의테스트 스터디원 제한
    - 출석부 스터디원 제한

--- module-user/src/main/java/org/example/domain/attendance/service/ListAttendanceService.java
+++ module-user/src/main/java/org/example/domain/attendance/service/ListAttendanceService.java
@@ -1,9 +1,16 @@
 package org.example.domain.attendance.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.api_response.exception.GeneralException;
+import org.example.api_response.status.ErrorStatus;
 import org.example.domain.attendance.controller.response.ListAttendanceResponse;
 import org.example.domain.attendance.repository.ListAttendanceRepository;
-import org.example.domain.week.repository.DetailWeekRepository;
+import org.example.domain.member.Member;
+import org.example.domain.member.service.CoreMemberService;
+import org.example.domain.study.Study;
+import org.example.domain.study.service.CoreStudyService;
+import org.example.domain.study_member.repository.StudyMemberRepository;
+import org.example.util.SecurityUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +19,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ListAttendanceService {
 
+  private final CoreMemberService coreMemberService;
+  private final CoreStudyService coreStudyService;
   private final ListAttendanceRepository listAttendanceRepository;
-  private final DetailWeekRepository detailWeekRepository;
+  private final StudyMemberRepository studyMemberRepository;
 
   public ListAttendanceResponse getAttendanceList(Long studyId) {
+    Member member = coreMemberService.findByEmail(SecurityUtils.getCurrentMemberEmail());
+    Study study = coreStudyService.findById(studyId);
+    if (studyMemberRepository.findByStudyAndMember(study, member).isEmpty()) {
+      throw new GeneralException(ErrorStatus.BAD_REQUEST, "스터디원만 열람할 수 있습니다.");
+    }
     return ListAttendanceResponse.builder()
       .attendanceList(listAttendanceRepository.getAttendanceList(studyId))
       .build();
--- module-user/src/main/java/org/example/domain/workbook/service/ListWorkbookService.java
+++ module-user/src/main/java/org/example/domain/workbook/service/ListWorkbookService.java
@@ -6,13 +6,17 @@ import org.example.api_response.exception.GeneralException;
 import org.example.api_response.status.ErrorStatus;
 import org.example.domain.controller.response.ListWorkbookDto;
 import org.example.domain.controller.response.ListWorkbookResponse;
+import org.example.domain.member.Member;
+import org.example.domain.member.service.CoreMemberService;
 import org.example.domain.problem.Level;
 import org.example.domain.study.Study;
 import org.example.domain.study.enums.StudyType;
 import org.example.domain.study.service.CoreStudyService;
+import org.example.domain.study_member.repository.StudyMemberRepository;
 import org.example.domain.workbook.repository.ListWorkbookRepository;
 import org.example.domain.workbook_problem.controller.response.ListWorkbookProblemDto;
 import org.example.domain.workbook_problem.repository.ListWorkbookProblemRepository;
+import org.example.util.SecurityUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +25,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ListWorkbookService {
 
+  private final CoreMemberService coreMemberService;
   private final CoreStudyService coreStudyService;
   private final ListWorkbookRepository listWorkbookRepository;
   private final ListWorkbookProblemRepository listWorkbookProblemRepository;
+  private final StudyMemberRepository studyMemberRepository;
 
   /**
    * 정규 스터디 모의테스트 조회
@@ -32,6 +38,10 @@ public class ListWorkbookService {
     Study study = coreStudyService.findById(studyId);
     if (study.getType().equals(StudyType.TEMP)) {
       throw new GeneralException(ErrorStatus.BAD_REQUEST, "정규 스터디가 아닙니다.");
+    }
+    Member member = coreMemberService.findByEmail(SecurityUtils.getCurrentMemberEmail());
+    if (studyMemberRepository.findByStudyAndMember(study, member).isEmpty()) {
+      throw new GeneralException(ErrorStatus.BAD_REQUEST, "스터디원만 열람할 수 있습니다.");
     }
 
     List<ListWorkbookDto> workbookList = listWorkbookRepository.getWorkbookList(study);
