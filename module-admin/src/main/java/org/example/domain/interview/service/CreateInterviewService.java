1ccf8b8c38858308e9305901afc15020ce0becfe
Kim Doo Hyeon <engus525@naver.com>
2024-08-07 21:19:21 +0900

    [FIX] 8/6 QA 반영 #198
    - 면접 일정 중복 시 에러

--- module-admin/src/main/java/org/example/domain/interview/service/CreateInterviewService.java
+++ module-admin/src/main/java/org/example/domain/interview/service/CreateInterviewService.java
@@ -3,11 +3,13 @@ package org.example.domain.interview.service;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.api_response.exception.GeneralException;
 import org.example.api_response.status.ErrorStatus;
 import org.example.domain.interview.Interview;
 import org.example.domain.interview.controller.request.UpdateInterviewRequest;
+import org.example.domain.interview.repository.ListInterviewRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CreateInterviewService {
 
   private final CoreInterviewService coreInterviewService;
+  private final ListInterviewRepository listInterviewRepository;
 
   /**
    * 면접 일정 수정
@@ -28,7 +31,14 @@ public class CreateInterviewService {
       SimpleDateFormat inputFormat = new SimpleDateFormat("MMddHHmm");
       Date date = inputFormat.parse(request.time());
       SimpleDateFormat outputFormat = new SimpleDateFormat("M월 d일 HH:mm");
-      interview.updateTime(outputFormat.format(date));
+
+      List<String> interviewTimeList = listInterviewRepository.getInterviewTimeList();
+      String time = outputFormat.format(date);
+      if (interviewTimeList.contains(time)) {
+        throw new GeneralException(ErrorStatus.NOTICE_BAD_REQUEST, "면접 일정은 중복될 수 없습니다.");
+      }
+      interview.updateTime(time);
+
     } catch (ParseException e) {
       throw new GeneralException(ErrorStatus.INTERNAL_ERROR, "면접 일정 변환 중 오류가 발생했습니다.");
     }
