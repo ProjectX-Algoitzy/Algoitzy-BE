93494582ba380e07cbad3d51904b1ec23e4d2f43
sebin chu <cntpqls0218@gmail.com>
2024-08-16 11:36:47 +0900

    [FEATURE] KISA 파일 업로드 확장자 제한 #226
    - 이미지와 문서 파일 내용 확장자에 대한 FileType enum class 구현
    - FileType에 정의된 확장자만 요청할 수 있도록 개선

--- module-core/src/main/java/org/example/domain/s3_file/service/CoreCreateS3FileService.java
+++ module-core/src/main/java/org/example/domain/s3_file/service/CoreCreateS3FileService.java
@@ -17,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.api_response.exception.GeneralException;
 import org.example.api_response.status.ErrorStatus;
+import org.example.domain.s3_file.FileType;
 import org.example.domain.s3_file.S3File;
 import org.example.domain.s3_file.repository.S3FileRepository;
 import org.example.util.FileUtils;
@@ -45,26 +46,33 @@ public class CoreCreateS3FileService {
    */
   public List<String> uploadS3File(List<MultipartFile> multipartFileList) {
     List<String> fileUrlList = new ArrayList<>();
+
     for (MultipartFile multipartFile : multipartFileList) {
-      String fileName = generateRandomFileName(multipartFile.getOriginalFilename());
+      String originalFileName = multipartFile.getOriginalFilename();
+      String randomFileName = generateRandomFileName(originalFileName);
+      String fileExtension = getFileExtension(originalFileName);
+
+      if (!FileType.isSupported(fileExtension)) {
+          throw new GeneralException(ErrorStatus.NOTICE_BAD_REQUEST, "허용되지 않는 파일 형식입니다. 지원되는 형식을 확인하세요.");
+      }
 
       try (InputStream inputStream = multipartFile.getInputStream()) {
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentLength(multipartFile.getSize());
         objectMetadata.setContentType(multipartFile.getContentType());
-        PutObjectRequest request = new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+        PutObjectRequest request = new PutObjectRequest(bucket, randomFileName, inputStream, objectMetadata)
           .withCannedAcl(PublicRead);
         amazonS3.putObject(request);
       } catch (IOException e) {
         throw new GeneralException(ErrorStatus.INTERNAL_ERROR, "파일 업로드에 실패했습니다.");
       }
 
-      String fileUrl = amazonS3Client.getUrl(bucket, fileName).toString();
+      String fileUrl = amazonS3Client.getUrl(bucket, randomFileName).toString();
 
       s3FileRepository.save(
         S3File.builder()
           .originalName(multipartFile.getOriginalFilename())
-          .fileName(fileName)
+          .fileName(randomFileName)
           .fileUrl(fileUrl)
           .build());
       fileUrlList.add(fileUrl);
@@ -76,10 +84,10 @@ public class CoreCreateS3FileService {
   /**
    * 중복 방지를 위한 파일명 난수화
    */
-  private String generateRandomFileName(String originalName) {
+  private String generateRandomFileName(String originalFileName) {
     String fileName;
     do {
-      fileName = RandomUtils.getRandomString(16).concat(FileUtils.getFileExtension(originalName));
+      fileName = RandomUtils.getRandomString(16).concat(FileUtils.getFileExtension(originalFileName));
     } while (s3FileRepository.existsByFileName(fileName));
 
     return profile + File.separator + fileName;
@@ -89,14 +97,10 @@ public class CoreCreateS3FileService {
    * S3 파일 삭제
    */
   public void deleteS3File(String fileUrl) {
-
     String fileName = extractFileNameFromUrl(fileUrl);
     fileName = profile + File.separator + fileName;
 
-    // S3에서 파일 삭제
     amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
-
-    // 레포지토리에서 파일 삭제
     s3FileRepository.deleteByFileName(fileName);
   }
 
@@ -105,4 +109,16 @@ public class CoreCreateS3FileService {
     int fileIndex = fileUrl.lastIndexOf('/');
     return fileUrl.substring(fileIndex + 1);
   }
+
+  // 파일 확장자 추출
+  private String getFileExtension(String fileName) {
+    if (fileName != null && fileName.lastIndexOf('.') > 0) {
+      return fileName.substring(fileName.lastIndexOf('.') + 1);
+    }
+    return "";
+  }
+
+
+
+
 }
