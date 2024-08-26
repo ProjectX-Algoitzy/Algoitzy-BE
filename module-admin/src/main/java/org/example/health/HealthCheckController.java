b55c558f1973b1d3934a800db8d908555a3e8a40
Kim Doo Hyeon <engus525@naver.com>
2024-08-27 02:33:36 +0900

    [FEATURE] 프로덕션 서버 CI/CD #217
    - health check cors

--- module-admin/src/main/java/org/example/health/HealthCheckController.java
+++ module-admin/src/main/java/org/example/health/HealthCheckController.java
@@ -1,12 +1,14 @@
 package org.example.health;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
+    @CrossOrigin("*")
     @GetMapping("/health")
     @Operation(hidden = true)
     public String healthCheck() {
