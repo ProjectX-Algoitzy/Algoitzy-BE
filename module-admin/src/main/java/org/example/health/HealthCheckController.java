8a2645cf0370b84ad3cd62e9814cfacdcef7247d
Kim Doo Hyeon <engus525@naver.com>
2024-08-27 06:19:52 +0900

    [FEATURE] 프로덕션 서버 CI/CD #217
    - selenium check

--- module-admin/src/main/java/org/example/health/HealthCheckController.java
+++ module-admin/src/main/java/org/example/health/HealthCheckController.java
@@ -1,18 +1,34 @@
 package org.example.health;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.example.api_response.ApiResponse;
+import org.example.util.http_request.Url;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/health")
+@RequiredArgsConstructor
 public class HealthCheckController {
 
+    private final WebDriver webDriver;
+
     @CrossOrigin("*")
-    @GetMapping("/health")
+    @GetMapping()
     @Operation(hidden = true)
     public String healthCheck() {
         return "I'm healthy";
+    }
+
+    @GetMapping()
+    @Deprecated
+    @Operation(summary = "크롤링 테스트용", description = "반환값 : 김두현 백준 문제 풀이 수")
+    public ApiResponse<Integer> seleniumCheck() {
+        webDriver.get(Url.BAEKJOON_USER.getBaekjoonUserUrl("engus525"));
+        return ApiResponse.onSuccess(Integer.parseInt(webDriver.findElement(By.id("u-solved")).getText()));
     }
 
 }
