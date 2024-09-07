ef02f8533d2d1c0bb35b89b510a38af80e8fff9f
cobinding <cntpqls0218@gmail.com>
2024-09-07 15:52:56 +0900

    [FIX] 인증번호 요청 시, 같은 공유기인 경우 IP 같은 문제 해결 #292
    - 불필요한 import 제거

--- module-core/src/main/java/org/example/domain/sms/controller/SmsController.java
+++ module-core/src/main/java/org/example/domain/sms/controller/SmsController.java
@@ -2,14 +2,12 @@ package org.example.domain.sms.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.api_response.ApiResponse;
 import org.example.domain.sms.controller.request.ValidatePhoneNumberRequest;
 import org.example.domain.sms.service.SmsService;
 import org.example.domain.sms.controller.request.CertificationPhoneNumberRequest;
-import org.example.util.IpUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
