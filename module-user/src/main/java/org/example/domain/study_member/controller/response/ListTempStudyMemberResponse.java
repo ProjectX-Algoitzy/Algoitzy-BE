ba6956762b10f86d3e545e117944d953c3dda338
Kim Doo Hyeon <engus525@naver.com>
2024-09-18 22:52:57 +0900

    [FEATURE] USER 자율 스터디 - 스터디원 목록 #321
    - @Default 추가

--- module-user/src/main/java/org/example/domain/study_member/controller/response/ListTempStudyMemberResponse.java
+++ module-user/src/main/java/org/example/domain/study_member/controller/response/ListTempStudyMemberResponse.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Schema(description = "자율 스터디원 목록 응답 객체")
 public class ListTempStudyMemberResponse {
 
+  @Default
   @Schema(description = "자율 스터디원 목록")
   private List<ListTempStudyMemberDto> studyMemberList = new ArrayList<>();
 
