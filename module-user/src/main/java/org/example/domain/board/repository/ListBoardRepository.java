efeda74e2a98f2151778305b1930b3eefe55ce05
cobinding <cntpqls0218@gmail.com>
2024-10-22 17:31:29 +0900

    [FEATURE] 커뮤니티 USER 게시물 관련 API 개발 #349
    - 게시판 목록 조회 Repository(QueryDsl) 구현

--- module-user/src/main/java/org/example/domain/board/repository/ListBoardRepository.java
+++ module-user/src/main/java/org/example/domain/board/repository/ListBoardRepository.java
@@ -1,11 +1,24 @@
 package org.example.domain.board.repository;
 
+import static org.example.domain.board.QBoard.board;
+import static org.example.domain.board_like.QBoardLike.boardLike;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.board.enums.BoardCategory;
+import org.example.domain.board.enums.BoardSort;
+import org.example.domain.board.request.SearchBoardRequest;
 import org.example.domain.board.response.ListBoardDto;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,8 +29,79 @@ public class ListBoardRepository {
     /*
     * 게시판 목록 조회
     * */
-    public List<ListBoardDto> getBoardList(BoardCategory boardCategory) {
-
+    public List<ListBoardDto> getBoardList(SearchBoardRequest request) {
+        List<ListBoardDto> boardList =
+            queryFactory
+                .select(Projections.fields(ListBoardDto.class,
+                    board.id.as("boardId"),
+                    board.category.stringValue().as("boardCategory"),
+                    board.title,
+                    board.member.name.as("createdName"),
+                    board.createdTime,
+                    Expressions.booleanTemplate(
+                        "case when {0} > {1} then true else false end",
+                        board.createdTime, LocalDateTime.now().minusDays(3)).as("newBoardYn"),
+                    board.viewCount,
+                    board.fixYn,
+                    board.deleteYn
+                        )
+                    )
+                .from(board)
+                .leftJoin(boardLike).on(board.eq(boardLike.board))
+                .where(
+                    searchCategory(request.category()),
+                    searchKeyword(request.searchKeyword()),
+                    board.saveYn.isTrue()
+                )
+                .groupBy(board)
+                .offset(request.pageRequest().getOffset())
+                .limit(request.pageRequest().getPageSize())
+                .orderBy(
+                    new CaseBuilder()
+                        .when(board.fixYn.isTrue()).then(0)
+                        .otherwise(1).asc(),
+                    searchOrderBy(request.sort())
+                )
+                .fetch();
+        return boardList;
     }
 
+    /**
+     * 카테고리 검색
+     */
+    private static Predicate searchCategory(BoardCategory category) {
+        if (category == null) {
+            return null;
+        }
+        return board.category.eq(category);
+    }
+
+    /**
+     * 키워드 검색(제목, 내용, 작성자)
+     */
+    private Predicate searchKeyword(String searchKeyword) {
+        if (!StringUtils.hasText(searchKeyword)) {
+            return null;
+        }
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.or(board.title.contains(searchKeyword));
+        builder.or(board.content.contains(searchKeyword));
+        builder.or(board.member.name.contains(searchKeyword));
+        return builder;
+    }
+
+    /**
+     * 정렬 조건
+     */
+    private OrderSpecifier<?> searchOrderBy(BoardSort sort) {
+        if (sort == null) {
+            return board.createdTime.desc();
+        }
+
+        return switch (sort) {
+            case LATEST -> board.createdTime.desc();
+            case LIKE -> boardLike.count().desc();
+            case VIEW_COUNT -> board.viewCount.desc();
+        };
+    }
 }
