6ceacd482d8a3eac9dff9d5638c51e7a64658279
cobinding <cntpqls0218@gmail.com>
2024-11-04 21:42:46 +0900

    [FEATURE] 커뮤니티 USER 게시물 관련 API 개발 #349
    - 게시글 댓글 조회 API 구현 완료

--- module-user/src/main/java/org/example/domain/board/controller/BoardController.java
+++ module-user/src/main/java/org/example/domain/board/controller/BoardController.java
@@ -6,17 +6,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.api_response.ApiResponse;
 import org.example.domain.board.controller.request.SearchBoardRequest;
+import org.example.domain.reply.controller.request.SearchReplyRequest;
 import org.example.domain.board.controller.response.DetailBoardResponse;
 import org.example.domain.board.controller.response.ListBoardResponse;
-import org.example.domain.board.enums.BoardCategory;
+import org.example.domain.reply.controller.response.ListReplyResponse;
 import org.example.domain.board.controller.response.ListBoardCategoryResponse;
 import org.example.domain.board.service.BoardService;
+import org.example.domain.reply.service.ReplyService;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     private final BoardService boardService;
+    private final ReplyService replyService;
 
     @GetMapping("/category")
     @Operation(summary = "게시글 카테고리 목록 조회")
@@ -44,5 +46,13 @@ public class BoardController {
     @Operation(summary = "게시글 상세 조회")
     public ApiResponse<DetailBoardResponse> getDetailBoard(@PathVariable("board-id") Long boardId) {
         return ApiResponse.onSuccess(boardService.getDetailBoard(boardId));
+    }
+
+    @GetMapping("/{board-id}/reply")
+    @Operation(summary = "게시글 댓글 목록 조회")
+    public ApiResponse<ListReplyResponse> getReplyList(
+        @PathVariable("board-id") Long boardId,
+        @ParameterObject @ModelAttribute @Valid SearchReplyRequest request) {
+        return ApiResponse.onSuccess(replyService.getReplyList(boardId, request));
     }
 }
