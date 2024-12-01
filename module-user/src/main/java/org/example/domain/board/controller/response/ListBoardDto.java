bf0ca9f7c3e234adec35284931d802630ede1141
Kim Doo Hyeon <engus525@naver.com>
2024-12-01 21:07:17 +0900

    [FEATURE] 커뮤니티 기능 개발 release/1.4
    - 정규 스터디 이력 없는 경우 게시글 목록 조회 시 작성자 이름 미노출

--- module-user/src/main/java/org/example/domain/board/controller/response/ListBoardDto.java
+++ module-user/src/main/java/org/example/domain/board/controller/response/ListBoardDto.java
@@ -15,37 +15,41 @@ import org.example.domain.board.enums.BoardCategory;
 @Schema(description = "게시글 목록 조회 응답 DTO")
 public class ListBoardDto {
 
-    @Schema(description = "게시글 ID")
-    private long boardId;
+  @Schema(description = "게시글 ID")
+  private long boardId;
 
-    @Schema(description = "카테고리")
-    private String category;
+  @Schema(description = "카테고리")
+  private String category;
 
-    @Schema(description = "제목")
-    private String title;
+  @Schema(description = "제목")
+  private String title;
 
-    @Schema(description = "작성자")
-    private String createdName;
+  @Schema(description = "작성자")
+  private String createdName;
 
-    @Schema(description = "작성일")
-    private LocalDateTime createdTime;
+  @Schema(description = "작성일")
+  private LocalDateTime createdTime;
 
-    @Schema(description = "새 게시물 여부")
-    private boolean newBoardYn;
+  @Schema(description = "새 게시물 여부")
+  private boolean newBoardYn;
 
-    @Schema(description = "조회수")
-    private int viewCount;
+  @Schema(description = "조회수")
+  private int viewCount;
 
-    @Schema(description = "최종 저장 여부")
-    private boolean saveYn;
+  @Schema(description = "최종 저장 여부")
+  private boolean saveYn;
 
-    @Schema(description = "고정 여부")
-    private boolean fixYn;
+  @Schema(description = "고정 여부")
+  private boolean fixYn;
 
-    @Schema(description = "삭제 여부")
-    private boolean deleteYn;
+  @Schema(description = "삭제 여부")
+  private boolean deleteYn;
 
-    public void updateCategory(String type) {
-        this.category = BoardCategory.valueOf(type).getName();
-    }
+  public void updateCategory(String type) {
+    this.category = BoardCategory.valueOf(type).getName();
+  }
+
+  public void blindCreatedName() {
+    this.createdName = null;
+  }
 }
