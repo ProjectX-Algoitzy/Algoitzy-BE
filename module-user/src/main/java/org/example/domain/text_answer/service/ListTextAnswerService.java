4b98f9a26df8b299bc0c728183c65288c3e134bc
Kim Doo Hyeon <engus525@naver.com>
2024-12-26 16:36:23 +0900

    [FIX] USER 지원서 임시저장 이후 문항 노출되지 않는 오류 수정 #381

--- module-user/src/main/java/org/example/domain/text_answer/service/ListTextAnswerService.java
+++ module-user/src/main/java/org/example/domain/text_answer/service/ListTextAnswerService.java
@@ -3,9 +3,11 @@ package org.example.domain.text_answer.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.text_answer.controller.response.DetailTextAnswerDto;
+import org.example.domain.text_answer.repository.DetailTextAnswerRepository;
 import org.example.domain.text_answer.repository.ListTextAnswerRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class ListTextAnswerService {
 
   private final ListTextAnswerRepository listTextAnswerRepository;
+  private final DetailTextAnswerRepository detailTextAnswerRepository;
 
   /**
    * 지원서 상세 주관식 답변 목록 조회
    */
   public List<DetailTextAnswerDto> getTextAnswerList(Long applicationId, Long answerId) {
-    return listTextAnswerRepository.getTextAnswerList(applicationId, answerId);
+    List<DetailTextAnswerDto> textQuestionList = listTextAnswerRepository.getTextQuestionList(applicationId);
+    for (DetailTextAnswerDto dto : textQuestionList) {
+      String textAnswer = detailTextAnswerRepository.getTextAnswer(dto.getTextQuestionId(), answerId);
+      if (!StringUtils.hasText(textAnswer)) continue;
+      dto.setText(textAnswer);
+    }
+    return textQuestionList;
   }
 }
