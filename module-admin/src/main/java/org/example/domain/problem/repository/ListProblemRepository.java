5ff8fe7cbff592fd7613b20aa662fea7cea885e6
Kim Doo Hyeon <engus525@naver.com>
2025-01-15 18:19:33 +0900

    [REFACTOR] 심화반 모의테스트 문제 스터디원 고려하도록 수정 #411

--- module-admin/src/main/java/org/example/domain/problem/repository/ListProblemRepository.java
+++ module-admin/src/main/java/org/example/domain/problem/repository/ListProblemRepository.java
@@ -1,7 +1,6 @@
 package org.example.domain.problem.repository;
 
 import static org.example.domain.problem.QProblem.problem;
-import static org.example.domain.problem_algorithm.QProblemAlgorithm.problemAlgorithm;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
@@ -11,8 +10,6 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.example.domain.problem.Level;
-import org.example.domain.problem.Problem;
 import org.example.domain.problem.controller.request.SearchProblemRequest;
 import org.example.domain.problem.controller.response.ListProblemDto;
 import org.example.util.ArrayUtils;
@@ -27,21 +24,6 @@ import org.springframework.util.StringUtils;
 public class ListProblemRepository {
 
   private final JPAQueryFactory queryFactory;
-
-
-  public List<Problem> getProblemList(List<String> algorithmList) {
-    return queryFactory
-      .selectFrom(problem)
-      .innerJoin(problemAlgorithm).on(problem.eq(problemAlgorithm.problem))
-      .where(
-        problemAlgorithm.algorithm.englishName.in(algorithmList),
-        problem.level.in(Level.findByLevel(6, 13)) // SILVER5 ~ GOLD3
-      )
-      .groupBy(problem.number)
-      .orderBy(problem.solvedCount.desc())
-      .limit(5)
-      .fetch();
-  }
 
   /**
    * 백준 문제 목록 조회
