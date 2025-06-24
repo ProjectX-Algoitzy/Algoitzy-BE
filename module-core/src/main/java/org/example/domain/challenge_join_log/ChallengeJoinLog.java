d35f27a2485618bc69296b98c6738c84bb505dc1
Kim Doo Hyeon <engus525@naver.com>
2025-06-24 11:26:46 +0900

    [FEATURE] 데일리 챌린지 백준 크롤링 #465
    - JPA ChallengeJoinLog 누락 필드 추가

--- module-core/src/main/java/org/example/domain/challenge_join_log/ChallengeJoinLog.java
+++ module-core/src/main/java/org/example/domain/challenge_join_log/ChallengeJoinLog.java
@@ -1,5 +1,7 @@
 package org.example.domain.challenge_join_log;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
@@ -13,7 +15,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.config.jpa.BooleanToYNConverter;
+import org.example.domain.challenge_join_log.enums.LanguageType;
 import org.example.domain.challenge_problem.ChallengeProblem;
+import org.example.domain.member.Member;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -32,15 +37,38 @@ public class ChallengeJoinLog {
   @JoinColumn(name = "date")
   private ChallengeProblem challengeProblem;
 
-  @Comment("백준 닉네임")
-  private String handle;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Comment("실행 시간")
+  private Integer executionTime;
+
+  @Comment("메모리")
+  private Integer memory;
+
+  @Comment("코드 길이")
+  private Integer codeLength;
+
+  @Comment("언어")
+  private LanguageType languageType;
+
+  @Convert(converter = BooleanToYNConverter.class)
+  @Column(nullable = false, columnDefinition = "char(1) default 'N'")
+  @Comment("보상 변환 여부")
+  private Boolean convertYn;
 
   @LastModifiedDate
   private LocalDateTime updatedTime;
 
   @Builder
-  public ChallengeJoinLog(ChallengeProblem challengeProblem, String handle) {
+  public ChallengeJoinLog(ChallengeProblem challengeProblem, Member member,
+    Integer executionTime, Integer memory, Integer codeLength, LanguageType languageType) {
     this.challengeProblem = challengeProblem;
-    this.handle = handle;
+    this.member = member;
+    this.executionTime = executionTime;
+    this.memory = memory;
+    this.codeLength = codeLength;
+    this.languageType = languageType;
   }
 }
