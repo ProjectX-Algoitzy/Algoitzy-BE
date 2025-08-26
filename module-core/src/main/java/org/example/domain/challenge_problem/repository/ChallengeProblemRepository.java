763e34ad1f162970f2fa388074227a49d905ebf6
Kim Doo Hyeon <engus525@naver.com>
2025-08-26 15:24:36 +0900

    [FEATURE] 데일리 챌린지 리더보드 #469
    - 챌린지 문제 정보 조회 API

--- module-core/src/main/java/org/example/domain/challenge_problem/repository/ChallengeProblemRepository.java
+++ module-core/src/main/java/org/example/domain/challenge_problem/repository/ChallengeProblemRepository.java
@@ -1,9 +1,13 @@
 package org.example.domain.challenge_problem.repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import org.example.domain.challenge_problem.ChallengeProblem;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeProblemRepository extends JpaRepository<ChallengeProblem, LocalDate> {
 
+  @EntityGraph(attributePaths = {"problem"})
+  Optional<ChallengeProblem> findByDate(LocalDate date);
 }
