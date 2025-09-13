f3714c6cc2793342ee3362490e081b1162bbd1a0
Kim Doo Hyeon <engus525@naver.com>
2025-09-13 14:19:42 +0900

    [FEATURE] 데일리 챌린지 API 수정 #470
    - 출석부 조회 시 보상 사용 여부 필드 추가

--- module-user/src/main/java/org/example/domain/attendance/controller/response/ListAttendanceDto.java
+++ module-user/src/main/java/org/example/domain/attendance/controller/response/ListAttendanceDto.java
@@ -33,4 +33,13 @@ public class ListAttendanceDto {
 
   @Schema(description = "모의테스트 여부")
   private Boolean workbookYN;
+
+  @Schema(description = "문제 할당량 보상 사용 여부")
+  private Boolean problemRewardYn;
+
+  @Schema(description = "블로그 포스팅 보상 사용 여부")
+  private Boolean blogRewardYn;
+
+  @Schema(description = "모의테스트 보상 사용 여부")
+  private Boolean workbookRewardYn;
 }
